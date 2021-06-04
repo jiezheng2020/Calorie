c85e2d03cefdef1d31a6c17ba718888bfc96d0e1
Kevin Zheng <jiezheng2020@gmail.com>
2021-06-04 17:26:26 -0500

    getting current diary

--- react-app/src/components/Diary/Diary.js
+++ react-app/src/components/Diary/Diary.js
@@ -7,14 +7,33 @@ import "./Diary.css";
 
 const Diary = () => {
   const user = useSelector((state) => state.session.user);
-  const [currDate, setcurrDate] = useState(new Date());
+  const diaries = useSelector((state) => state.diaries);
+  const [currDiary, setCurrDiary] = useState([]);
+  const [currDate, setcurrDate] = useState(
+    new Date().toISOString().split("T")[0]
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchDiaries());
   }, [dispatch]);
 
-  return <div> Diary Page</div>;
+  useEffect(() => {
+    if (diaries.length) {
+      setCurrDiary(diaries.filter((diary) => diary.date.includes(currDate)));
+    }
+  }, [diaries, currDate]);
+
+  return (
+    <div>
+      <input
+        type="date"
+        value={currDate}
+        onChange={(e) => setcurrDate(e.target.value)}
+      ></input>
+      <div></div>
+    </div>
+  );
 };
 
 export default Diary;
