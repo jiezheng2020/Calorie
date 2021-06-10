54714aeb0ba42730d08d58607583f416517f7021
Kevin Zheng <jiezheng2020@gmail.com>
2021-06-10 17:19:02 -0500

    profile barebone implemented

--- react-app/src/components/Diary/Diary.js
+++ react-app/src/components/Diary/Diary.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
 import {
   fetchDiaries,
   createDiaries,
--- react-app/src/components/Profile/Profile.js
+++ react-app/src/components/Profile/Profile.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { fetchDiaries } from "../../store/diary";
 import "./Profile.css";
 
 const Profile = () => {
-  return <div>User Profile</div>;
+  const dispatch = useDispatch();
+  const [currDate, setcurrDate] = useState(
+    new Date().toISOString().split("T")[0]
+  );
+
+  const [currDiary, setCurrDiary] = useState([]);
+
+  useEffect(async () => {
+    const res = await dispatch(fetchDiaries(currDate));
+
+    await setCurrDiary(res[0]);
+  }, [dispatch, currDate]);
+
+  return (
+    <div className="auth-container">
+      <div className="profile-container">User Profile</div>
+    </div>
+  );
 };
 
 export default Profile;
